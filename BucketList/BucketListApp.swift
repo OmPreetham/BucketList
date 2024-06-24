1a1caed369de71b7ce4125c74030bcbfc332b146
Om Preetham <ompreethambandi@gmail.com>
2024-06-23 19:53:23 -0500

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Om Preetham Bandi on 6/23/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
